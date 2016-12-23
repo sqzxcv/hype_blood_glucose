3394108fa68a217a701c7a3876b407ef4509d6f5
ShengQiang <qiang_sheng@qq.com>
2016-12-23 14:49:49 +0800

    解决input不能接受事件的问题

--- javascript/tableviewcellAction.js
+++ javascript/tableviewcellAction.js
@@ -3,63 +3,62 @@ function morning_1action (hypeDocument, element, event) {
 	var div = hypeDocument.getElementById("compassViewTitleDiv");
 	div.innerHTML = '<img src="' + hypeDocument.resourcesFolderURL() + '/morning_img_2x.png" height="29" width="45" ><br /><font  class="compassViewTitleDiv"> 早晨 — 空腹</font>';
 
-	console.log(div.innerHTML);
+	registerDocEventListener();
 }
 
 function morning_2action (hypeDocument, element, event) {
 	// body...
 	var div = hypeDocument.getElementById("compassViewTitleDiv");
 	div.innerHTML = '<img src="' + hypeDocument.resourcesFolderURL() + '/morning_img_2x.png" height="29" width="45" ><br /><font  class="compassViewTitleDiv"> 早晨 — 饭后</font>';
-	console.log(div.innerHTML);
+	registerDocEventListener();
 }
 
 function moon_1action (hypeDocument, element, event) {
 	// body...
 	var div = hypeDocument.getElementById("compassViewTitleDiv");
 	div.innerHTML = '<img src="' + hypeDocument.resourcesFolderURL() + '/moon_2x.png" height="31" width="37" ><br /><font  class="compassViewTitleDiv"> 中午 — 饭前</font>';
-	console.log(div.innerHTML);
+	registerDocEventListener();
 }
 
 function moon_2action (hypeDocument, element, event) {
 	// body...
 	var div = hypeDocument.getElementById("compassViewTitleDiv");
 	div.innerHTML = '<img src="' + hypeDocument.resourcesFolderURL() + '/moon_2x.png" height="31" width="37" ><br /><font  class="compassViewTitleDiv"> 中午 — 饭后</font>';
-	console.log(div.innerHTML);
+	registerDocEventListener();
 }
 
 function night_1action (hypeDocument, element, event) {
 	// body...
 	var div = hypeDocument.getElementById("compassViewTitleDiv");
 	div.innerHTML = '<img src="' + hypeDocument.resourcesFolderURL() + '/night_2x.png" height="32" width="37" ><br /><font  class="compassViewTitleDiv"> 晚上 — 饭前</font>';
-	console.log(div.innerHTML);
+	registerDocEventListener();
 }
 
 function night_2action (hypeDocument, element, event) {
 	// body...
 	var div = hypeDocument.getElementById("compassViewTitleDiv");
 	div.innerHTML = '<img src="' + hypeDocument.resourcesFolderURL() + '/night_2x.png" height="32" width="37" ><br /><font  class="compassViewTitleDiv"> 晚上 — 饭后</font>';
-	console.log(div.innerHTML);
+	registerDocEventListener();
 }
 
 function midnight_1action (hypeDocument, element, event) {
 	// body...
 	var div = hypeDocument.getElementById("compassViewTitleDiv");
 	div.innerHTML = '<img src="' + hypeDocument.resourcesFolderURL() + '/midnight_2x.png" height="26" width="38" ><br /><font  class="compassViewTitleDiv"> 睡前</font>';
-	console.log(div.innerHTML);
+	registerDocEventListener();
 }
 
 function midnight_2action (hypeDocument, element, event) {
 	// body...
 	var div = hypeDocument.getElementById("compassViewTitleDiv");
 	div.innerHTML = '<img src="' + hypeDocument.resourcesFolderURL() + '/midnight_2x.png" height="26" width="38" ><br /><font  class="compassViewTitleDiv">凌晨</font>';
-	console.log(div.innerHTML);
+	registerDocEventListener();
 }
 
 function midnight_3action (hypeDocument, element, event) {
 	// body...
 	var div = hypeDocument.getElementById("compassViewTitleDiv");
 	div.innerHTML = '<img src="' + hypeDocument.resourcesFolderURL() + '/midnight_2x.png" height="26" width="38" ><br /><font  class="compassViewTitleDiv"> 其他</font>';
-	console.log(div.innerHTML);
+	registerDocEventListener();
 }
 
-//<img src="${resourcesFolderName}/morning_img@2x.png">
